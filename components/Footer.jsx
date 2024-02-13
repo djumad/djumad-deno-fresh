6977c32393635b4f76d6d67f092e98bbe342145b
djumad <42491296+djumad@users.noreply.github.com>
2024-02-13 14:00:26 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () => {
     <div>
       <footer class="w3-center w3-black w3-padding-16">
         <p>
-          Powered by{" "}
+          Rancangan Footer{" "}
           <a
             href="https://www.w3schools.com/w3css/default.asp"
             title="W3.CSS"
             target="_blank"
             class="w3-hover-text-green"
           >
-            w3.css
+            By Deno Fresh
           </a>
         </p>
       </footer>
